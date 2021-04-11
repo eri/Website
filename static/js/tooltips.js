82a5d8df2c1a3e76705af9cbf9e8d02bf4e2b80f
eri <contact.erionline@gmail.com>
2021-04-12 00:15:04 +0200

    new: Add tippy for tooltips on hover

---
+++ static/js/tooltips.js
@@ -0,0 +1,40 @@
+tippy("#email", {
+    content: "Click to email me",
+    placement: 'right',
+    arrow: false,
+  });
+
+tippy("#discord", {
+  content: "Eri#1000",
+  placement: 'bottom',
+});
+
+tippy("#twitter", {
+  content: "@erionline",
+  placement: 'bottom',
+});
+
+tippy("#github", {
+  content: "eri",
+  placement: 'bottom',
+});
+
+tippy("#linkedin", {
+  content: "Eray Can Ozdemir",
+  placement: 'bottom',
+});
+
+tippy("#french", {
+  content: "French (native)",
+  placement: 'bottom',
+});
+
+tippy("#english", {
+  content: "English (B1)",
+  placement: 'bottom',
+});
+
+tippy("#turkish", {
+  content: "Turkish (native)",
+  placement: 'bottom',
+});
