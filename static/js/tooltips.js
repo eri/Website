9b21206a206970f5f0cd476d2aef71ba887853cc
eri <contact.erionline@gmail.com>
2021-04-12 15:37:41 +0200

    new: Added tippy for loading status icon & Medium

--- static/js/tooltips.js
+++ static/js/tooltips.js
@@ -1,40 +1,51 @@
+tippy("#status-icon", {
+  content: "Loading...",
+  placement: "right",
+  arrow: false,
+});
+
 tippy("#email", {
-    content: "Click to email me",
-    placement: 'right',
-    arrow: false,
-  });
+  content: "Click to email me",
+  placement: "right",
+  arrow: false,
+});
 
 tippy("#discord", {
   content: "Eri#1000",
-  placement: 'bottom',
+  placement: "bottom",
 });
 
 tippy("#twitter", {
   content: "@erionline",
-  placement: 'bottom',
+  placement: "bottom",
 });
 
 tippy("#github", {
   content: "eri",
-  placement: 'bottom',
+  placement: "bottom",
 });
 
 tippy("#linkedin", {
   content: "Eray Can Ozdemir",
-  placement: 'bottom',
+  placement: "bottom",
+});
+
+tippy("#medium", {
+  content: "Eray C.",
+  placement: "bottom",
 });
 
 tippy("#french", {
   content: "French (native)",
-  placement: 'bottom',
+  placement: "bottom",
 });
 
 tippy("#english", {
   content: "English (B1)",
-  placement: 'bottom',
+  placement: "bottom",
 });
 
 tippy("#turkish", {
   content: "Turkish (native)",
-  placement: 'bottom',
+  placement: "bottom",
 });
