ff83432d0baa341b72396bdbee2b13bfe9aa0d89
eri <contact.erionline@gmail.com>
2021-04-20 15:36:39 +0200

    new: Run file to enable/disable SSR support

--- run.py
+++ run.py
@@ -1,11 +1,35 @@
+# Import app & constants
+from src.website import app
+from src import constants
+
 # Import Frozen-Flask, a static site generator
 from flask_frozen import Freezer
 
-# Import my Flask App
-from website import app
 
-freezer = Freezer(app)
+if constants.ssr == True:
+    """ 
+    Server Side Rendering is active
 
-if __name__ == '__main__':
-    # Run the initial Flask app
-    freezer.freeze()
+    This is useful if we need a static version of the site
+    to deploy in a web server or to Netlify.
+
+    Static files will be saved in a new /build directory
+
+    """
+    freezer = Freezer(app)
+
+    if __name__ == '__main__':
+        # Run the initial Flask app
+        freezer.freeze()
+
+else:
+    """
+    Server Side Rendering is disabled
+    
+    The app will be executed and run by a Flask server
+    to be served dynamically.
+
+    """
+    if __name__ == '__main__':
+        app.run(debug=False, host=constants.host, port=constants.port)
+    
