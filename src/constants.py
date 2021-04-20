ce10ac1be5f3eef3c92ca3b94d8d39593292e42e
eri <contact.erionline@gmail.com>
2021-04-20 15:37:49 +0200

    new: Move all global constants to separate file

---
+++ src/constants.py
@@ -0,0 +1,98 @@
+# Main constants
+ssr = False
+host = "0.0.0.0"  # Used if ssr (Server Side Rendering) is False
+port = "8000"  # Used if ssr (Server Side Rendering) is False
+
+# Configuration
+discord_id = "187316528100802560"
+
+me = {
+    "name": "Eray C.",
+    "email": "eray@eri.gg",
+    "location": "Paris, France",
+    "school": "ESTIAM Paris (Bac+2)",
+    "bio": "My name is Eray, I'm a 20 years old student living in France. My biggest goal is to learn and improve my current programmation skills during my 5 years of academic courses. I wish to find a job later as a web or application developer.",
+}
+
+# Metadata
+main_metadata = {
+    "locale": "en_US",
+    "author": "eri.gg",
+    "title": "Eray C.",
+    "desc": "Random French student learning some code, messing around with Python and writing stories on Medium.",
+    "image": "https://media.discordapp.net/attachments/712969229506183198/831109920103465000/img_caching.png",
+    "favicon": "https://images-ext-2.discordapp.net/external/5Hi7Y5E0rvFTKpKPeGg6Uc1p1-itFQPf4AwEsjzcUqo/https/i.imgur.com/tGibRPY.png",
+}
+
+social_metadata = {
+    "discord": {
+        "desc": "Find out my Discord account named to add me as friend!",
+        "url": f"https://discord.com/users/{discord_id}",
+    },
+    "github": {
+        "desc": "Discover my work and projects from my GitHub profile.",
+        "url": "https://github.com/eri",
+    },
+    "linkedin": {
+        "desc": "Check out my LinkedIn profile to know more about my skills & experiences.",
+        "url": "https://linkedin.com/in/erayc",
+    },
+    "medium": {
+        "desc": "Read my diverse Medium stories about my personal life, interests and other topics.",
+        "url": "https://erionline.medium.com",
+    },
+    "twitter": {
+        "desc": "Follow me on Twitter to stay informed about what I do or feel.",
+        "url": "https://twitter.com/erionline",
+    },
+}
+
+# Main page - Experiences
+experiences = [
+    {
+        "name": "Discord Templates",
+        "position": "Founder & Developer",
+        "link": "https://discordtemplates.com?utm_source=eri.gg",
+        "time": "Since June 2020",
+    },
+    {
+        "name": "Top.gg",
+        "position": "Website Moderator",
+        "link": "https://top.gg?utm_source=eri.gg",
+        "time": "June 2019 > April 2021",
+    },
+    {
+        "name": "SARL Ayhan Restaurant",
+        "position": "Website Developer",
+        "link": None,
+        "time": "July 2020 > August 2020",
+    },
+]
+
+# Main page - Education
+education = [
+    {
+        "name": "ESTIAM Paris",
+        "description": "Computer Science School applied to Businesses",
+        "link": "https://www.estiam.education/en/?utm_source=eri.gg",
+        "time": "Since October 2019",
+    },
+    {
+        "name": "HS Henri Sellier",
+        "description": "Technological Bachelor",
+        "link": "http://www.lycee-henri-sellier.fr/?utm_source=eri.gg",
+        "time": "September 2017 > July 2019",
+    },
+]
+
+# Main page - Technologies
+technologies = [
+    {"name": "Python", "color": "blue-500", "size": "full"},
+    {"name": "Flask", "color": "gray-700", "size": "2/3"},
+    {"name": "Django", "color": "green-700", "size": "1/4"},
+    {"name": "Javascript", "color": "yellow-600", "size": "1/4"},
+    {"name": "HTML5", "color": "red-700", "size": "1/2"},
+    {"name": "PHP", "color": "indigo-500", "size": "1/6"},
+    {"name": "MongoDB", "color": "green-900", "size": "2/4"},
+    {"name": "Tailwind CSS", "color": "green-500", "size": "1/3"},
+]
