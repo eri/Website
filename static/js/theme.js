c73d036ea324b4f401c3854dd04fc8d27a374005
merloss <54235902+Merloss@users.noreply.github.com>
2022-05-02 22:18:30 +0300

    fixed change theme issue

--- static/js/theme.js
+++ static/js/theme.js
@@ -1,48 +1,45 @@
 // HTML elements
-const main = document.getElementById("page")
-const switcher = document.getElementById("themeButton")
+const main = document.getElementById("page");
+const switcher = document.getElementById("themeButton");
 
 // Settings
 const storedTheme = localStorage.theme;
 
 // Events
-switcher.onclick = themeEvent();
 document.onload = () => {
-    if (!storedTheme) changeTheme(storedTheme);
+  if (!storedTheme) changeTheme(storedTheme);
 };
 
-const themeEvent = () => {
-    // Get the current theme and change it
+switcher.onclick = () => {
+  // Get the current theme and change it
 
-    if (main.classList.value.includes("dark")) {
-        changeTheme("light")
-        storeTheme("light")
-        switcher._tippy.setContent(`Dark theme`);
-    } else {
-        changeTheme("dark")
-        storeTheme("dark")
-        switcher._tippy.setContent(`Light theme`);
-    }
+  if (main.classList.value.includes("dark")) {
+    changeTheme("light");
+    storeTheme("light");
+    switcher._tippy.setContent(`Dark theme`);
+  } else {
+    changeTheme("dark");
+    storeTheme("dark");
+    switcher._tippy.setContent(`Light theme`);
+  }
 
-    // Define a default theme if not set
-    if (!storedTheme) storeTheme("dark");
-    
-}
+  // Define a default theme if not set
+  if (!storedTheme) storeTheme("dark");
+};
 
 const storeTheme = (theme) => {
-    try {
-        localStorage.theme = theme;
-        return true
-    } catch (e) {
-        console.error(e)
-        return false
-    } 
-}
+  try {
+    localStorage.theme = theme;
+    return true;
+  } catch (e) {
+    console.error(e);
+    return false;
+  }
+};
 
 const changeTheme = (theme) => {
-    // Enables the dark mode 
-    if (theme == "dark") document.getElementById("page").classList.add("dark")
-    
-    // Enables the light mode
-    else document.getElementById("page").classList.remove("dark")
-}
+  // Enables the dark mode
+  if (theme == "dark") document.getElementById("page").classList.add("dark");
+  // Enables the light mode
+  else document.getElementById("page").classList.remove("dark");
+};
